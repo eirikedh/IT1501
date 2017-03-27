b746a83a8afe9cd9e925b7b54778a5c0134ea646
eirikedh <eirik.kopperud.edh@gmail.com>
2017-03-27 13:59:31 +0200

    Update SonarLEDBorderDetect.ino

--- Arduino/SonarLEDBorderDetect.ino
+++ Arduino/SonarLEDBorderDetect.ino
@@ -4,81 +4,140 @@
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
 
-#define LED 13
 
 // this might need to be tuned for different lighting conditions, surfaces, etc.
 #define QTR_THRESHOLD  1800 // 
-  
-// these might need to be tuned for different motor types
-#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
-#define TURN_SPEED        200
+
+#define LED 13
 #define FORWARD_SPEED     100
-#define REVERSE_DURATION  200 // ms
-#define TURN_DURATION     300 // ms
-#define LEFT 1  // turn direction
-#define RIGHT 2
-#define SONAR_RANGE       30 // in cm
+#define SONAR_RANGE 50 // in cm
+#define NUM_SENSORS 6
 
 const int echoPin = 2; // GRØNN KABEL 
 const int triggerPin = 3; // GUL KABEL 
 const int ledPin = A1; // ORANSJE KABEL
 const int maxDistance = 20; // Limit for action in cm
+char val;
 
-
-#define NUM_SENSORS 6
 unsigned int sensor_values[NUM_SENSORS];
-// Initialize "sonar" with trigger pin, echo pin and max distance,
-// and then servo, motor, button and IR-sensor array
-NewPing sonar(triggerPin, echoPin, SONAR_RANGE);
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON); 
+// Initialize "sonar" with trigger pin, echo pin and sonar range
+NewPing sonar(triggerPin, echoPin, SONAR_RANGE);
 // Tell Zumo that we have no emitter-pin
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN); 
- 
- 
+
+
 void setup() {
    pinMode(ledPin,OUTPUT);
+   sensors.init();
    button.waitForButton();
    motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
+   Serial.begin(9600);
 }
 
 void loop() {
+  if(Serial.available()) {
+    val = Serial.read();
+  }
+
+  if (val == 'd') {
+    checkDistance();
+  }
+  
+  if (val == 's') {
+    checkSensor();
+  }
+  
+  if (val == 'a') {
+    checkDistanceSensor();
+  }
+  
+  //checkDistance();
+  //checkSensor();
+  //checkDistanceSensor();
+   
+}
+
+
+void checkDistance() {
   // Read distance to obstacle, if any 
   unsigned int time = sonar.ping();
   float distance = sonar.convert_cm(time);
-
-  // check if inside range for action
-  if (! distance > 0) { // sonar gives zero when outside range
-    // If no object in front, turn around untill detecting
-    digitalWrite(ledPin,LOW); 
+  // check if inside range for action  
+  if (!distance > 0) { // No object in front
+    digitalWrite(ledPin, LOW); 
     motors.setSpeeds(-2*FORWARD_SPEED, 2*FORWARD_SPEED);
   } 
-  
-  else if (distance > 0) {
-    // If object detected
+  else if (distance > 0) { // Object detected
     digitalWrite(ledPin,HIGH);
     // Drive towards object
     motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
-  }  
- 
+  }
+}
+
+
+void checkSensor() {
   // Read IR-sensors and check if border detected
   sensors.read(sensor_values);
   if (sensor_values[0] > QTR_THRESHOLD) {
-    // if leftmost sensor detects line, reverse and turn to the right
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    sensor0();
   }
-  
-  else if (sensor_values[5] > QTR_THRESHOLD) {
-    // if rightmost sensor detects line, reverse and turn to the left
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  if (sensor_values[5] > QTR_THRESHOLD) {
+    sensor5();
+  }
+  if (!(sensor_values[0] > QTR_THRESHOLD) and !(sensor_values[0] > QTR_THRESHOLD)) {
+    motors.setSpeeds(-2*FORWARD_SPEED, 2*FORWARD_SPEED);
+  }
+}
+
+
+void checkDistanceSensor() {
+  sensors.read(sensor_values);
+  unsigned int time = sonar.ping();
+  float distance = sonar.convert_cm(time);
+  // No object in front or sensor value detected
+  if (!(distance > 0) and !(sensor_values[0] > QTR_THRESHOLD) and !(sensor_values[5] > QTR_THRESHOLD)) { 
+    digitalWrite(ledPin, LOW); 
+    motors.setSpeeds(-2*FORWARD_SPEED, 2*FORWARD_SPEED);
+  } 
+  // Object detected
+  else if ((distance > 0) and !(sensor_values[0] > QTR_THRESHOLD) and !(sensor_values[5] > QTR_THRESHOLD)) { 
+    digitalWrite(ledPin,HIGH);
+    motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
+  }
+  // Sensor_value[0] detects
+  else if ((!distance > 0) and (sensor_values[0] > QTR_THRESHOLD) and (!sensor_values[5] > QTR_THRESHOLD)) {
+    digitalWrite(ledPin, LOW); 
+    sensor0();
+  }
+  // Sensor_value[5] detects
+  else if ((!distance > 0) and (!sensor_values[0] > QTR_THRESHOLD) and (sensor_values[5] > QTR_THRESHOLD)) {
+    digitalWrite(ledPin, LOW); 
+    sensor5();  
+  }
+  else {
+    digitalWrite(ledPin, LOW); 
+    motors.setSpeeds(-2*FORWARD_SPEED, 2*FORWARD_SPEED);
   }
 
 }
+
+void sensor0() {
+  // if leftmost sensor detects line, reverse and turn to the right
+  motors.setSpeeds(-FORWARD_SPEED, -FORWARD_SPEED);
+  delay(FORWARD_SPEED);
+  motors.setSpeeds(FORWARD_SPEED, -FORWARD_SPEED);   
+  delay(FORWARD_SPEED);
+  motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED); 
+}
+
+void sensor5() {
+  // if rightmost sensor detects line, reverse and turn to the left
+  motors.setSpeeds(-FORWARD_SPEED, -FORWARD_SPEED);
+  delay(FORWARD_SPEED);
+  motors.setSpeeds(-FORWARD_SPEED, FORWARD_SPEED);
+  delay(FORWARD_SPEED);
+  motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+}
+
